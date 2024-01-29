ae29f363a691abffaf65ee1263506ba5ffc5600d
InterPolar <118571561+BruhDevel@users.noreply.github.com>
2024-01-29 16:19:52 -0300

    Update PalindromeNumber.cpp

--- Solutions/Easy/PalindromeNumber.cpp
+++ Solutions/Easy/PalindromeNumber.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
  bool isPalindrome(int x) {
 	bool Value = true;
-	string X = "";
-	string X2 = "";
-	for (char a : std::to_string(x)) {
-		X +=a;
-	}
-	X2 = X;
+	string X = std::to_string(x);
+	string X2 = X;
 	std::reverse(X2.begin(), X2.end());
 	if (X != X2) {
 		Value = false;
