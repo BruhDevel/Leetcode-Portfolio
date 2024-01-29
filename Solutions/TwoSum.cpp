bd5b8dd83a5577847cb3e005ee64ce48b050b139
InterPolar <118571561+BruhDevel@users.noreply.github.com>
2024-01-29 13:39:18 -0300

    Create TwoSum.cpp

---
+++ Solutions/TwoSum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+	for (int i = 0; i < nums.size(); i++) {
+		for (int i2 = 0; i2 < nums.size(); i2++) {
+			if (i != i2) {
+				if (nums[i] + nums[i2] == target) {	
+					return { i,i2 };
+				}
+			}
+		}
+	}
+    return {0,0};
+  }
+};
